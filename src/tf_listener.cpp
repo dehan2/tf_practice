b6c8c12016579ae38baba0af72f586cd8404bccc
Jehyun Cha <rolika4316@gmail.com>
2023-07-11 13:51:28 +0900

    Listener added

--- src/tf_listener.cpp
+++ src/tf_listener.cpp
@@ -37,9 +37,15 @@ int main(int argc, char** argv)
 			ros::Duration(1.0).sleep();
 			continue;
 		}
+
+		geometry_msgs::Twist vel_msg;
+
+		vel_msg.angular.z = 4.0 * atan2(transformStamped.transform.translation.y, transformStamped.transform.translation.x);
+		vel_msg.linear.x = 0.5 * sqrt(pow(transformStamped.transform.translation.x, 2) + pow(transformStamped.transform.translation.y, 2));
+		turtle_vel.publish(vel_msg);
+
+		rate.sleep();
 	}
 
-	geometry_msgs::Twist vel_msg;
-
-	vel_msg.angular.z = 4.0 * atan2(transformStamped.transform.translation.y)
+	return 0;
 }
